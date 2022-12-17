9432ea387ac75b7e98a1051793bb1c0dc6c5e6ec
Santiago Palladino <spalladino@gmail.com>
2022-12-16 23:05:26 -0300

    Adds jsdoc for config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,25 @@
 export interface NoirConfig {
+  /**
+   * Path to the Noir circuits (defaults to circuits).
+   */
   circuitsPath: string;
+  /**
+   * Name of the main circuit, used to generate the acir build artifact and 
+   * verifier contract (defaults to main).
+   */
   mainCircuitName: string;
+  /**
+   * Path to the nargo binary (defaults to nargo).
+   */
   nargoBin: string;
+  /**
+   * Whether to automatically call noir:compile when hardhat compile is
+   * invoked (defaults to true, requires nargo).
+   */
   autoCompile: boolean;
+  /**
+   * Whether to automatically call noir:contract when hardhat compile is
+   * invoked (defaults to true, requires nargo).
+   */
   autoGenerateContract: boolean;
 }
